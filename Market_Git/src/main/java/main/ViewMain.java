197050ebbcfd96f3567511af9e9658af411d9e01
hojoon <tory37@naver.com>
2014-04-01 20:39:25 +0900

    Commit2

--- Market_Git/src/main/java/main/ViewMain.java
+++ Market_Git/src/main/java/main/ViewMain.java
@@ -16,7 +16,44 @@ public class ViewMain {
 	 */
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
+		MarketDAO.deleteAll();
+		DealDAO.deleteAll();
+		MarketDAO.insert(new Market(1001, "아이스크림", 1000, 0));
+		MarketDAO.insert(new Market(1002, "삼겹살", 6000, 0));
+		MarketDAO.insert(new Market(1003, "아메리카노", 5000, 0));
+		MarketDAO.insert(new Market(1004, "마우스", 10000, 0));
 		
+		DealDAO.dealIn(new Deal(101, 1001,3));
+		DealDAO.dealIn(new Deal(102, 1003,5));
+		DealDAO.dealIn(new Deal(103, 1004,6));
+		
+		DealDAO.dealOut(new Deal(104, 1004,6));
+		DealDAO.dealOut(new Deal(105, 1004,6));
+		
+		DealDAO.delete(103);
+		//System.out.println(DealDAO.getDeal(104));
+		/*
+		ArrayList<Deal> temp = null;
+		temp = DealDAO.getDealAll();
+		
+		for(Deal d : temp){
+			System.out.println(d);
+		}
+		*/
+		BuyListDAO.deleteAll();
+		BuyListDAO.insert(new BuyList(1, "1"));
+		BuyListDAO.insert(new BuyList(2, "2"));
+		BuyListDAO.insert(new BuyList(3, "3"));
+		BuyListDAO.insert(new BuyList(4, "4"));
+		
+		System.out.println(BuyListDAO.getBuyList(1));
+		
+		ArrayList<BuyList> temp = null;
+		temp = (ArrayList)BuyListDAO.getBuyListAll();
+		
+		for(BuyList d : temp){
+			System.out.println(d);
+		}
 	}
 
 }
